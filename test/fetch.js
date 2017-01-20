749ff2dc4877f66add2f6230b2aa2ded732111d4
Alex Dumat <alex@trak.local>
2017-01-20 15:40:46 +0100

    starting test mocha

---
+++ test/fetch.js
@@ -0,0 +1,38 @@
+var expect = require("chai").expect;
+var request = require("request");
+var url = "http://localhost:3000/";
+
+describe("Page access test", function() {
+
+  describe("Fetching home page", function() {
+    it("Home page", function() {
+	    url = url+ "home";
+    	it("returns status 200", function() {
+      		request(url, function(error, response, body) {
+        		expect(response.statusCode).to.equal(200);
+      		});
+    	});
+    });});
+
+    describe("Fetching about page", function() {
+    it("About page", function() {
+        url = url+ "about";
+        it("returns status 200", function() {
+                request(url, function(error, response, body) {
+                        expect(response.statusCode).to.equal(200);
+                });
+        });
+    });});
+
+    describe("Fetching contact page", function() {
+    it("Contact page", function() {
+        url = url+ "contact";
+        it("returns status 200", function() {
+                request(url, function(error, response, body) {
+                        expect(response.statusCode).to.equal(200);
+                });
+        });
+    });});
+
+  });
+
