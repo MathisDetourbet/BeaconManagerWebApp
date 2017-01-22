3b50370f8c457f2d2169f4b6752ce300d0d6935c
Mathis Detourbet <mathis.detourbet@gmail.com>
2017-01-22 18:47:26 +0100

    Database manager class created, addContent + addBeacon + beaconsList + contentsList: DONE

--- custom_middleware/auth_check.js
+++ custom_middleware/auth_check.js
@@ -4,6 +4,7 @@ function checkAuth(req, res, next) {
 	if (!req.session.user_id) {
 		req.flash('info', 'Session has expired. Please log in.');
 		res.redirect('login');
+		
 	} else {
 		next();
 	}
