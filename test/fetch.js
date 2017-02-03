3773551ce433ca23afabc16ea6c656783dd86478
Alex Dumat <alex@ax.local>
2017-02-03 16:30:53 +0100

    OK

--- test/fetch.js
+++ test/fetch.js
@@ -1,6 +1,7 @@
 var expect = require("chai").expect;
 var request = require("request");
 var url = "http://localhost:3000/";
+var app = require('../app.js');
 
 describe("Page access test without login", function() {
     it("Home page", function() {
@@ -21,6 +22,31 @@ describe("Page access test without login", function() {
     });
     it("Contact page", function() {
         url = url+ "contact";
+        it("returns status 200", function() {
+                request(url, function(error, response, body) {
+                        expect(response.statusCode).to.equal(200);
+                });
+        });
+    });
+  });
+
+describe("Page access test with login", function() {
+    it("Login", function() {
+	    url = url+ "login";
+      var chai = require('chai');
+      var chaiHttp = require('chai-http');
+      chai.use(chaiHttp);
+      chai.request(app)
+          .put('/login')
+          .send({username: 'admin@admin.com', passsword: 'efreitr'})
+          .end(function(err, res) {
+              expect(err).to.be.null;
+              expect(res).to.have.status(200);
+          });
+
+    });
+    it("Sign in", function() {
+        url = url+ "registration";
         it("returns status 200", function() {
                 request(url, function(error, response, body) {
                         expect(response.statusCode).to.equal(200);
